8fd668756807db375c0407fe5ceb5d3965c241af
Geordan Rosario <geordan@gmail.com>
2016-05-04 22:40:50 +0800

    Attacker/defender-specific modifications.

--- attack/modification/all.go
+++ attack/modification/all.go
@@ -1,16 +1,24 @@
 package modification
 
 import (
+	"github.com/geordanr/go_xwing/constants"
 	"github.com/geordanr/go_xwing/interfaces"
 )
+
+var attackerSpendFocus = SpendFocus{actor: constants.ATTACKER}
+var defenderSpendFocus = SpendFocus{actor: constants.DEFENDER}
+var attackerRollDice = RollDice{actor: constants.ATTACKER}
+var defenderRollDice = RollDice{actor: constants.DEFENDER}
 
 var All = map[string]interfaces.Modification{
 	// compareresults.go
 	"Compare Results": &CompareResults{},
 	// focus.go
-	"Spend Focus": &SpendFocus{},
+	"Attacker Spend Focus": &attackerSpendFocus,
+	"Defender Spend Focus": &defenderSpendFocus,
 	// roll.go
-	"Roll Dice": &RollDice{},
+	"Roll Attack Dice": &attackerRollDice,
+	"Roll Defense Dice": &defenderRollDice,
 	// sufferdamage.go
 	"Suffer Damage": &SufferDamage{},
 }
