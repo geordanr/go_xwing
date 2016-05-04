a876bb419b509904ef545e184ad875636035c4dd
Geordan Rosario <geordan@gmail.com>
2016-05-04 22:40:16 +0800

    gofmt; better name for histogram Integers.

--- dice/dice.go
+++ dice/dice.go
@@ -1,19 +1,19 @@
 package dice
 
 func RollAttackDice(numDice uint8) Results {
-    results := make(Results, numDice)
-    for i := range(results) {
-	results[i] = new(AttackDie)
-	results[i].Roll()
-    }
-    return results
+	results := make(Results, numDice)
+	for i := range results {
+		results[i] = new(AttackDie)
+		results[i].Roll()
+	}
+	return results
 }
 
 func RollDefenseDice(numDice uint8) Results {
-    results := make(Results, numDice)
-    for i := range(results) {
-	results[i] = new(DefenseDie)
-	results[i].Roll()
-    }
-    return results
+	results := make(Results, numDice)
+	for i := range results {
+		results[i] = new(DefenseDie)
+		results[i].Roll()
+	}
+	return results
 }
--- histogram/histogram.go
+++ histogram/histogram.go
@@ -1,46 +1,50 @@
 package histogram
 
 import (
-    "fmt"
-    "sort"
-    "strings"
+	"fmt"
+	"sort"
+	"strings"
 )
 
-type IntHistogram map[int]int
+type Integers map[int]int
 
-func (h IntHistogram) Normalized() map[int]float64 {
-    r := make(map[int]float64)
-    total := 0
-    for _, v := range(h) { total += v }
-    for k, v := range(h) {
-	r[k] = float64(v) / float64(total)
-    }
-    return r
+func (h Integers) Normalized() map[int]float64 {
+	r := make(map[int]float64)
+	total := 0
+	for _, v := range h {
+		total += v
+	}
+	for k, v := range h {
+		r[k] = float64(v) / float64(total)
+	}
+	return r
 }
 
-func (h IntHistogram) String() (str string) {
-    n := h.Normalized()
+func (h Integers) String() (str string) {
+	n := h.Normalized()
 
-    keys := []int{}
-    for k, _ := range(h) { keys = append(keys, int(k)) }
-    sort.Ints(keys)
-    for _, k := range(keys) {
-	str += fmt.Sprintf("%2d (%6.2f%%): %s\n", k, 100*n[k], strings.Repeat("#", int(50 * n[k])))
-    }
-    return
+	keys := []int{}
+	for k, _ := range h {
+		keys = append(keys, int(k))
+	}
+	sort.Ints(keys)
+	for _, k := range keys {
+		str += fmt.Sprintf("%2d (%6.2f%%): %s\n", k, 100*n[k], strings.Repeat("#", int(50*n[k])))
+	}
+	return
 }
 
-func (h IntHistogram) ToStrMap() map[string]int {
-    r := make(map[string]int)
-    for k, v := range(h) {
-	r[fmt.Sprintf("%d", k)] = v
-    }
-    return r
+func (h Integers) ToStrMap() map[string]int {
+	r := make(map[string]int)
+	for k, v := range h {
+		r[fmt.Sprintf("%d", k)] = v
+	}
+	return r
 }
 
-func (this *IntHistogram) Add(that IntHistogram) *IntHistogram {
-    for k, v := range(that) {
-	(*this)[k] += v
-    }
-    return this
+func (this *Integers) Add(that Integers) *Integers {
+	for k, v := range that {
+		(*this)[k] += v
+	}
+	return this
 }
