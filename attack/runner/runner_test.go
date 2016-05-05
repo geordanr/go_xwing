ff42e51e86f31bae1381964fdd9d53db6c9ad2ad
Geordan Rosario <geordan@gmail.com>
2016-05-05 13:44:30 +0800

    Remove contrived test.
    (No longer valid as default modifications now mess with the results.)

--- attack/runner/runner_test.go
+++ attack/runner/runner_test.go
@@ -6,61 +6,12 @@ import (
 	"github.com/geordanr/go_xwing/attack/modification"
 	"github.com/geordanr/go_xwing/attack/step"
 	"github.com/geordanr/go_xwing/constants"
-	"github.com/geordanr/go_xwing/dice"
 	"github.com/geordanr/go_xwing/gamestate"
 	"github.com/geordanr/go_xwing/interfaces"
 	"github.com/geordanr/go_xwing/ship"
-	"github.com/stretchr/testify/assert"
+	// "github.com/stretchr/testify/assert"
 	"testing"
 )
-
-func TestRun_ContrivedFocus(t *testing.T) {
-	assert := assert.New(t)
-
-	attacker := ship.New("Attacker", 0, 0, 0, 0)
-	defender := ship.New("Defender", 0, 0, 0, 0)
-	state := gamestate.GameState{}
-	attackerSpendFocus := modification.SpendFocus{}
-	attackerSpendFocus.SetActor(constants.ATTACKER)
-	mods := map[string][]interfaces.Modification{
-		"Modify Attack Dice": []interfaces.Modification{
-			&attackerSpendFocus,
-		},
-	}
-	runner := New(step.All, 1)
-	output := make(chan interfaces.GameState)
-	go runner.Run(output)
-
-	attacker.SetFocusTokens(2)
-	attackResults := dice.RollAttackDice(3)
-	attackResults[0].SetResult(dice.BLANK)
-	attackResults[1].SetResult(dice.FOCUS)
-	attackResults[2].SetResult(dice.HIT)
-	state.SetAttackResults(&attackResults)
-
-	defender.SetFocusTokens(3)
-	defenseResults := dice.RollDefenseDice(3)
-	defenseResults[0].SetResult(dice.BLANK)
-	defenseResults[1].SetResult(dice.FOCUS)
-	defenseResults[2].SetResult(dice.EVADE)
-	state.SetDefenseResults(&defenseResults)
-
-	state.EnqueueAttack(attack.New(attacker, defender, mods))
-
-	runner.InjectState(&state)
-	<-output
-
-	assert.EqualValues(1, attacker.FocusTokens())
-	assert.EqualValues(1, attackResults.Blanks())
-	assert.EqualValues(0, attackResults.Focuses())
-	assert.EqualValues(2, attackResults.Hits())
-	assert.EqualValues(0, attackResults.Crits())
-
-	assert.EqualValues(3, defender.FocusTokens())
-	assert.EqualValues(1, defenseResults.Blanks())
-	assert.EqualValues(1, defenseResults.Focuses())
-	assert.EqualValues(1, defenseResults.Evades())
-}
 
 func TestRun_MultipleStates(t *testing.T) {
 	// assert := assert.New(t)
