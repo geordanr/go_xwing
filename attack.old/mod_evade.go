e2038a9abef15910584efe7975f0e28a3b58450a
Geordan Rosario <geordan@gmail.com>
2016-05-05 12:21:59 +0800

    Go format stuff.

--- attack.old/mod_evade.go
+++ attack.old/mod_evade.go
@@ -1,22 +1,23 @@
 package attack
 
 import (
-    "github.com/geordanr/go_xwing/dice"
+	"github.com/geordanr/go_xwing/dice"
 )
 
-type useEvadeToken struct {}
+type useEvadeToken struct{}
+
 func (useEvadeToken) Modify(atk *Attack) *Attack {
-    if atk.Defender.EvadeTokens > 0 && (atk.AttackResults.Hits() + atk.AttackResults.Crits()) > (atk.DefenseResults.Evades()) {
-	atk.Defender.EvadeTokens--
-	evadeDie := new(dice.DefenseDie)
-	evadeDie.SetResult(dice.EVADE)
-	evadeDie.Lock()
-	results := *atk.DefenseResults
-	results = append(results, evadeDie)
-	atk.DefenseResults = &results
-    }
-    return atk
+	if atk.Defender.EvadeTokens > 0 && (atk.AttackResults.Hits()+atk.AttackResults.Crits()) > (atk.DefenseResults.Evades()) {
+		atk.Defender.EvadeTokens--
+		evadeDie := new(dice.DefenseDie)
+		evadeDie.SetResult(dice.EVADE)
+		evadeDie.Lock()
+		results := *atk.DefenseResults
+		results = append(results, evadeDie)
+		atk.DefenseResults = &results
+	}
+	return atk
 }
-func (useEvadeToken) String() string { return "Use Evade Token" }
-func (useEvadeToken) ModifiesAttackResults() bool { return false }
+func (useEvadeToken) String() string               { return "Use Evade Token" }
+func (useEvadeToken) ModifiesAttackResults() bool  { return false }
 func (useEvadeToken) ModifiesDefenseResults() bool { return true }
