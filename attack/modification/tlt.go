368901403d79eac753e000d960eb9e21b4f2252e
Geordan Rosario <geordan@gmail.com>
2016-05-06 19:15:41 +0800

    TLT should only do 1 damage if it hits.

--- attack/modification/tlt.go
+++ attack/modification/tlt.go
@@ -35,6 +35,19 @@ func (mod *TwinLaserTurret) ModifyState(state interfaces.GameState, ship interfa
 		mods["Roll Attack Dice"] = []interfaces.Modification{&rollTLTDice}
 	}
 
+	// Instead of the default deal damage handler, use our own
+	dealDamageMods := mods["Deal Damage"]
+	for i, mod := range dealDamageMods {
+		switch mod.(type) {
+		case *DealDamage:
+			dealDamageMods[i] = &DealTLTDamage{}
+		}
+	}
+	if len(dealDamageMods) == 0 {
+		// just in case
+		mods["Deal Damage"] = []interfaces.Modification{&DealTLTDamage{}}
+	}
+
 	// Signal the default Perform Attack Twice handler to activate
 	state.SetPerformAttackTwice(true)
 }
@@ -43,3 +56,17 @@ func (mod TwinLaserTurret) Actor() constants.ModificationActor          { return
 func (mod *TwinLaserTurret) SetActor(actor constants.ModificationActor) {}
 func (mod TwinLaserTurret) String() string                              { return "Twin Laser Turret" }
 func (mod TwinLaserTurret) IsSecondaryWeapon() bool                     { return true }
+
+// DealTLTDamage is installed by TwinLaserTurret and deals one damage to the defender if the attack hit.
+type DealTLTDamage struct{}
+
+func (mod *DealTLTDamage) ModifyState(state interfaces.GameState, ship interfaces.Ship) {
+	currentAttack := state.CurrentAttack()
+	if state.HitsLanded()+state.CritsLanded() > 0 {
+		currentAttack.Defender().SufferDamage(1)
+	}
+}
+func (mod DealTLTDamage) Actor() constants.ModificationActor          { return constants.DEFENDER }
+func (mod *DealTLTDamage) SetActor(actor constants.ModificationActor) {}
+func (mod DealTLTDamage) String() string                              { return "Deal TLT Damage" }
+func (mod DealTLTDamage) IsSecondaryWeapon() bool                     { return false }
